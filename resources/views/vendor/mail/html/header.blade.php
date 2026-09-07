625c6c342f27b08619faecb366d9bde5c824b984
Jorge Gouveia <jmvgouveia@gmail.com>
2026-09-07 16:03:41 +0100

    templateemail

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-<img src="{{ asset('images/maestro-logo-light.svg') }}" class="logo" alt="Maestro">
+<img src="{{ asset('images/maestro-logo-light.svg') }}" width="220" height="108" alt="Maestro" style="display: block; width: 220px; height: 108px; max-width: 220px; margin: 0 auto;">
 </a>
 </td>
 </tr>
