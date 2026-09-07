b9b5ba5acd488b2df45794bc535de085e20da6d1
Jorge Gouveia <jmvgouveia@gmail.com>
2026-09-07 09:30:37 +0100

    proxy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Middleware\TrustProxies;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,6 +12,16 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        if ($trustedProxies = env('TRUSTED_PROXIES')) {
+            $middleware->trustProxies(
+                at: array_map('trim', explode(',', $trustedProxies)),
+                headers: TrustProxies::HEADER_X_FORWARDED_FOR
+                    | TrustProxies::HEADER_X_FORWARDED_HOST
+                    | TrustProxies::HEADER_X_FORWARDED_PORT
+                    | TrustProxies::HEADER_X_FORWARDED_PROTO,
+            );
+        }
+
         $middleware->prependToPriorityList(
             \Illuminate\Contracts\Auth\Middleware\AuthenticatesRequests::class,
             \App\Http\Middleware\EnforceMfa::class,
