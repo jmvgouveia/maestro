42b0513340bd69704b99be6edc15e5fcff3c59e4
Jorge Gouveia <jmvgouveia@gmail.com>
2026-09-21 14:29:14 +0100

    exportweekday

--- app/Filament/Resources/ScheduleResource.php
+++ app/Filament/Resources/ScheduleResource.php
@@ -118,7 +118,7 @@ class ScheduleResource extends Resource
                 } else {
                     foreach ($schedule->classes as $class) {
                         $linha = [
-                            $schedule->id_weekday + 2,
+                            $schedule->id_weekday + 1,
                             $schedule->id_timeperiod,
                             "\"{$class->name}\"",
                             $class->year,
@@ -137,7 +137,7 @@ class ScheduleResource extends Resource
 
                 foreach ($turmaAlunos as $turma => $info) {
                     $linha = [
-                        $schedule->id_weekday + 2,
+                        $schedule->id_weekday + 1,
                         $schedule->id_timeperiod ,
                         "\"$turma\"",
                         $info['ano'],
